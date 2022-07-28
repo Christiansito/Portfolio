b921f2f68a43fe45c04a989c83c1fdbef3024e18
Chcalvanese <ch.calvanese@gmail.com>
2022-07-28 12:34:19 -0400

    Added Excel, Tableau and R proyects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
+import IMG1 from '../../assets/portfolio1.jfif'
 import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.jpg'
+import IMG3 from '../../assets/portfolio3.jpeg'
+import IMG4 from '../../assets/portfolio4.png'
+import IMG5 from '../../assets/portfolio5.jpg'
+import EXC from '../../assets/Excel_dashboard.xlsx'
+import cap from '../../assets/Google_Capstone_Proyect.rar'
 
 //cambiar imagenes en portfolio
 
@@ -13,44 +14,39 @@ const data = [
   {
     id: 1,
     image: IMG1, 
-    title: 'Advanced Dashboard in excel',
-    github: '',
-    demo: 'http'
+    title: 'Dynamic Dashboard in excel',
+    github: 'https://github.com/Christiansito/Portfolio_proyects/tree/main/Excel_dashboard',
+    demo: '',
+    download: EXC
   },
   {
     id: 2,
     image: IMG2, 
-    title: 'ReactJs Games SPA',
-    github: '',
-    demo: 'http'
+    title: 'Google Analytics Capston Proyect',
+    github: 'https://github.com/Christiansito/Portfolio_proyects/tree/main/Google_Capstone_Proyect',
+    demo: '',
+    download: cap
   },
   {
     id: 3,
     image: IMG3, 
-    title: 'Data Scrapping with SQL',
-    github: '',
-    demo: 'http'
-  },
-  {
-    id: 4,
-    image: IMG4, 
-    title: 'Data Scrapping with R',
-    github: '',
-    demo: 'http'
+    title: 'Tableau Public Data Visualizations',
+    github: 'https://github.com/Christiansito/Portfolio_proyects/tree/main/Google_Capstone_Proyect',
+    demo: 'https://public.tableau.com/app/profile/christian.calvanese/viz/Google_Capstone_Proyect_Clean/casual_per_weekday'
   },
   {
     id: 5,
-    image: IMG5, 
-    title: 'Tableau Viz',
-    github: '',
+    image: IMG4, 
+    title: 'Data Scrapping with SQL',
+    github: 'https://github.com/Christiansito/Portfolio_proyects',
     demo: 'http'
   },
   {
     id: 6,
-    image: IMG6, 
+    image: IMG5, 
     title: 'Data Scrapping with Pyhton',
-    github: '',
-    demo: 'http'
+    github: 'https://github.com/Christiansito/Portfolio_proyects',
+    demo: 'fttp'
   },
 ]
 
@@ -62,7 +58,7 @@ const Portfolio = () => {
 
       <div className='container portfolio__container'>
         {
-          data.map(({id, image, title, github, demo}) => {
+          data.map(({id, image, title, github, demo, download}) => {
             return(
               <article key={id} className='portfolio__item'>
                 <div className="portfolio__item-image">
@@ -71,7 +67,8 @@ const Portfolio = () => {
                   <h3>{title}</h3>
                   <div className='portfolio__item-cta'>
                     <a href={github} className='btn'>Github</a>
-                    <a href={demo} className='btn btn-primary' target={'_blank'} rel="noreferrer">Live Demo</a>
+                  {demo[0] === 'h' ? <a href={demo} className='btn btn-primary' target={'_blank'} rel="noreferrer">Live Demo</a> : 
+                  <a href={download} download className='btn btn-primary' target={'_blank'} rel="noreferrer">Download</a>}
                   </div>
              </article>
             )
